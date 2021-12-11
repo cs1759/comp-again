edb5d69165047e783a6b1014c911c66443552843
cs1759 <63865245+cs1759@users.noreply.github.com>
2021-12-11 09:33:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var compiler = require('compilex');
+var fs= require('fs');
+const md5 = require('md5');
+
+var app = express();
+app.use(bodyParser());
+
+var option = {stats : true};
+compiler.init(option);
+app.get('/' , function (req , res ) {
+	res.sendfile( __dirname + "/index.html");
+});
+
+app.post('/compilecode' , function (req , res ) {
+    
+	var code = req.body.code;	
+	var input = req.body.input;
+    var inputRadio = req.body.inputRadio;
+    var lang = req.body.lang;
+    if((lang === "C") || (lang === "C++"))
+    {        
+        if(inputRadio === "true")
+        {    
+        	var envData = { OS : "linux" , cmd : "gcc"};	   	
+        	compiler.compileCPPWithInput(envData , code ,input , function (data) {
+        		if(data.error)
+        		{
+        			res.send(data.error);    		
+        		}
+        		else
+        		{
+        			res.send(data.output);
+        		}
+        	});
+	   }
+	   else
+	   {
+	   	
+	   	var envData = { OS : "linux" , cmd : "gcc"};	   
+        	compiler.compileCPP(envData , code , function (data) {
+        	if(data.error)
+        	{
+        		res.send(data.error);
+        	}    	
+        	else
+        	{
+        		res.send(data.output);
+        	}
+    
+            });
+	   }
+    }
+    if( lang === "Python")
+    {
+        if(inputRadio === "true")
+        {
+            var envData = { OS : "windows"};
+            compiler.compilePythonWithInput(envData , code , input , function(data){
+               fs.writeFile("output.txt", data.output, (error)=>{
+
+                });
+                const md5Current = md5(fs.readFileSync('output.txt'));
+                const orig= md5(fs.readFileSync('stored.txt'));
+                if(md5Current === orig)
+                res.send("yesss");
+                else{
+                    res.send("No");
+                }
+            });            
+        }
+        else
+        {
+            var envData = { OS : "linux"};
+            compiler.compilePython(envData , code , function(data){
+                console.log("blah")
+                res.send(data.output);
+            });
+        }
+    }
+
+});
+
+app.get('/fullStat' , function(req , res ){
+    compiler.fullStat(function(data){
+        res.send(data);
+    });
+});
+
+app.listen(3000);
